a8075534a79beb727419933a2e6e5dec6a1d0d0b
Liping Marlow <limarlow@microsoft.com>
2020-02-22 13:41:38 -0600

    add cosmosdb

---
+++ GetRating/index.js
@@ -0,0 +1,17 @@
+module.exports = async function (context, req, ratingItems) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.ratingId || (req.body && req.body.ratingId)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Found " + ratingItems.userNotes
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+    context.done();
+};
