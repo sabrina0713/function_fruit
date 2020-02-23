25b3e70d54be4f2e186b7cf8a59454a7f601ac87
Liping Marlow <limarlow@microsoft.com>
2020-02-22 20:18:08 -0600

    add the query

--- CreateRating/index.js
+++ CreateRating/index.js
@@ -2,14 +2,12 @@ var request = require('request');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    var productExists=false
-    var userExists=false
+    
     if (req.query.userId || (req.body && req.body.userId)) {
-        //get User
-        //var dataUser = "{\"userId\": "+req.body.userId+"}";
+        
         var options = {
           'method': 'GET',
-          'url': 'https://serverlessohuser.trafficmanager.net/api/GetUser?userId='+req.body.userId,
+          'url': 'https://serverlessohuser.trafficmanager.net/api/GetUser?userId='+(req.query.userId || req.body.userId),
           'headers': {
             'Content-Type': 'application/x-www-form-urlencoded'
           },
@@ -32,24 +30,25 @@ module.exports = async function (context, req) {
                   console.log("productid"+req.body.productId)
                       
                         var createRating= 
-                        //"{\"id\": " + create_UUID 
-                       "{\"userId\": " + (req.query.userId || req.body.userId)
+                       
+                      /* "{\"userId\": " + (req.query.userId || req.body.userId)
                       + "{\"proudctId\": " + (req.query.productId || req.body.productId)
                       + "{\"rating\": " + ((req.query.rating || req.body.rating))
                       + "{\"userNotes\": " + ((req.query.userNotes || req.body.userNotes))
                       + "{\"timestamp\": " +  new Date().toLocaleString()
-                      //locationName
+                      */
+                   
                       context.res = {
                           // status: 200, /* Defaults to 200 */
                           body: createRating
 
                       };
 
-                      context.bindings.inputDocumentOut = createRating
+                      context.bindings.inputDocumentOut = req.query
                       //context.done();
                       }
 
-                    }
+    }
                   
            
     else {
@@ -59,12 +58,3 @@ module.exports = async function (context, req) {
         };
     }
 };
-function create_UUID(){
-    var dt = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = (dt + Math.random()*16)%16 | 0;
-        dt = Math.floor(dt/16);
-        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
-    });
-    return uuid;
-}
