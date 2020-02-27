859eed0818b279ba4562f5776610351a73258e74
Liping Marlow <limarlow@microsoft.com>
2020-02-27 16:24:18 -0600

    cosmosdb save events

---
+++ EventHubTrigger/index.js
@@ -0,0 +1,8 @@
+module.exports = async function (context, eventHubMessages) {
+    context.log(`JavaScript eventhub trigger function called for message array ${eventHubMessages}`);
+    
+    eventHubMessages.forEach((message, index) => {
+        context.log(`Processed message ${message}`);
+        context.bindings.inputDocumentOut = message
+    });
+};
