a97c1f79dc8d82b96e1f179dfbbc198ea72cc998
Liping Marlow <limarlow@microsoft.com>
2020-02-29 16:00:05 -0600

    md for getrating

--- GetRating/index.js
+++ GetRating/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, req, inputDocumentOut) {
     context.log('JavaScript HTTP trigger function processed a request.');
     
-        context.log("Found ToDo item, Description=" + inputDocumentOut.body);
+        context.log("Found ToDo item, Description=" + inputDocumentOut);
     
 
    
